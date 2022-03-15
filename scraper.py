986052e111f0cc621891daf955f964337fa177a0
ikhtiaralam <ikhtiar.alam@derbysoft.net>
2022-03-15 06:59:30 -0500

    add other examples to search

--- scraper.py
+++ scraper.py
@@ -39,3 +39,47 @@ class Scraper():
 wiki = Scraper()
 
 wiki.scrapdata()
+
+# Abstract example ##########################################################
+
+# import requests
+ 
+# subject = 'React (JavaScript library)'
+# url = 'https://en.wikipedia.org/w/api.php'
+# params = {
+#         'action': 'query',
+#         'format': 'json',
+#         'titles': subject,
+#         'prop': 'extracts',
+#         'exintro': True,
+#         'explaintext': True,
+#     }
+ 
+# response = requests.get(url, params=params)
+# data = response.json()
+ 
+# page = next(iter(data['query']['pages'].values()))
+# print(page['extract'][:50])
+
+
+
+
+# Make a search of a topic with srsearch ####################################
+
+# import requests
+ 
+# query = 'javascript'
+ 
+# url = 'https://en.wikipedia.org/w/api.php'
+# params = {
+#             'action':'query',
+#             'format':'json',
+#             'list':'search',
+#             'utf8':1,
+#             'srsearch':query
+#         }
+ 
+# data = requests.get(url, params=params).json()
+ 
+# for i in data['query']['search']:
+#     print(i['title'], ' - Word count: ', i['wordcount'])
